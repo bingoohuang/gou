58e2529c44f71ab6ef9396e016675ac41f0b4f7f
bingoo <bingoo.huang@gmail.com>
2018-03-13 16:45:18 +0800

    fix RowsAffected

--- sqlexecute.go
+++ sqlexecute.go
@@ -23,7 +23,7 @@ func ExecuteSql(db *sql.DB, oneSql string, maxRows int) ExecuteSqlResult {
 	if !IsQuerySql(oneSql) {
 		r, err := db.Exec(oneSql)
 		var affected int64 = 0
-		if err != nil && r != nil {
+		if r != nil {
 			affected, _ = r.RowsAffected()
 		}
 		return ExecuteSqlResult{Error: err, CostTime: time.Since(start), RowsAffected: affected, IsQuerySql: false}
