932828c666a37c5b622cff466d40e9cb3ad2cd3e
bingoo <bingoo.huang@gmail.com>
2018-07-08 16:45:09 +0800

    ExecuteCommandTimeout

--- shell.go
+++ shell.go
@@ -2,9 +2,12 @@ package go_utils
 
 import (
 	"bufio"
+	"bytes"
+	"context"
 	"io"
 	"os/exec"
 	"strings"
+	"time"
 )
 
 // 执行Shell脚本，返回行解析对象数组
@@ -46,4 +49,39 @@ func ExecuteBash(shellScripts string) (string, error) {
 	})
 
 	return stdout, err
+}
+
+func BashTimeout(cmds string, timeout time.Duration) (string, string) {
+	return ExecuteCommandTimeout(timeout, "bash", "-c", cmds)
+}
+
+// https://medium.com/@vCabbage/go-timeout-commands-with-os-exec-commandcontext-ba0c861ed738
+func ExecuteCommandTimeout(timeout time.Duration, name string, args ...string) (string, string) {
+	// Create a new context and add a timeout to it
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel() // The cancel should be deferred so resources are cleaned up
+
+	cmd := exec.CommandContext(ctx, name, args...)
+
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	var eout bytes.Buffer
+	cmd.Stderr = &eout
+
+	err := cmd.Run()
+	if err != nil {
+		return "", err.Error()
+	}
+
+	// We want to check the context error to see if the timeout was executed.
+	// The error returned by cmd.Output() will be OS specific based on what
+	// happens when a process is killed.
+	if ctx.Err() == context.DeadlineExceeded {
+		return out.String(), "timed out"
+	}
+
+	output := out.String()
+	eoutput := eout.String()
+
+	return output, eoutput
 }
