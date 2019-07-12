60cde53ac79d90b5a18934d4c397932e68f7ba4e
bingoohuang <bingoo.huang@gmail.com>
2019-07-12 10:47:45 +0800

    merge functions to gonet

--- net.go
+++ net.go
@@ -1,18 +1,9 @@
 package gou
 
 import (
-	"net"
-	"net/url"
-
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 )
 
-// IsIP 判断 host 字符串表达式是不是IP(v4/v6)的格式
-func IsIP(host string) bool {
-	ip := net.ParseIP(host)
-	return ip != nil
-}
 
 // RspBase 代表返回的公共结构
 type RspBase struct {
@@ -37,84 +28,3 @@ func ErrHandled(err error, c *gin.Context) bool {
 	return true
 }
 
-// BuildURL 创建一个url
-func BuildURL(base string, querys map[string]string) (string, error) {
-	u, err := url.Parse(base)
-	if err != nil {
-		return "", err
-	}
-	q := u.Query()
-
-	for k, v := range querys {
-		q.Set(k, v)
-	}
-
-	u.RawQuery = q.Encode()
-	return u.String(), nil
-}
-
-// RestGet 发起一次HTTP GET调用，并且反序列化JSON到v代表的指针中。
-func RestGetV2(url string, v interface{}, setting *UrlHttpSettings) error {
-	logrus.Debugf("RestGet %v", url)
-	req, err := UrlGetV2(url, setting)
-	if err != nil {
-		logrus.Warnf(" UrlUrlGet failed %v", err)
-		return err
-	}
-
-	if err := req.ToJson(v); err != nil {
-		logrus.Warnf("Unmarshal failed %v", err)
-		return err
-	}
-	logrus.Debugf("RestGet  Result %+v", v)
-
-	return nil
-}
-
-// RestGet 发起一次HTTP GET调用，并且反序列化JSON到v代表的指针中。
-func RestGet(url string, v interface{}) error {
-	return RestGetV2(url, v, nil)
-}
-
-// HTTPGet 表示一次HTTP的Get调用
-func HTTPGet(url string) ([]byte, error) {
-	logrus.Debugf("http get %s", url)
-
-	resp, err := UrlGet(url)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.Bytes()
-}
-
-// RestPost 表示一次HTTP的POST调用
-func RestPost(url string, requestBody interface{}, responseStruct interface{}) ([]byte, error) {
-	return RestPostFn(url, requestBody, responseStruct, nil)
-}
-
-func RestPostFn(url string, requestBody interface{}, responseStruct interface{}, fn func(_ *UrlHttpRequest) error) ([]byte, error) {
-	logrus.Debugf("post body %#v", requestBody)
-
-	resp, err := UrlPost(url)
-	if err != nil {
-		return nil, err
-	}
-
-	if fn != nil {
-		err = fn(resp)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if err = resp.JsonBody(requestBody); err != nil {
-		return nil, err
-	}
-
-	if responseStruct != nil {
-		return nil, resp.ToJson(responseStruct)
-	}
-
-	return resp.Bytes()
-}
--- util_test.go
+++ util_test.go
@@ -8,15 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIsIP(t *testing.T) {
-	a := assert.New(t)
-	a.True(IsIP("192.168.0.1"), "192.168.0.1是IPv4地址")
-	a.True(IsIP("FE80::0202:B3FF:FE1E:8329"), "FE80::0202:B3FF:FE1E:8329是IPv6地址")
-	a.True(IsIP("2001:db8::68"), "2001:db8::68是IPv6地址")
-	a.False(IsIP("http://[2001:db8:0:1]:80"), "http://[2001:db8:0:1]:80不是IP地址")
-	a.False(IsIP("app01"), "app01不是IP地址")
-	a.False(IsIP("app.01"), "app.01不是IP地址")
-}
 
 func TestTpl(t *testing.T) {
 	a := assert.New(t)
