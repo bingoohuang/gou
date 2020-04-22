0d4d7c555215ff8b510011bd5861c3d8113df29b
bingoohuang <bingoo.huang@gmail.com>
2020-04-22 12:41:32 +0800

    Closef

--- lang/err.go
+++ lang/err.go
@@ -10,5 +10,10 @@ import (
 // Closef runs function and on error return error by argument including the given error (usually
 // from caller function).
 func Closef(err *error, closer io.Closer, format string, a ...interface{}) {
-	*err = multierror.Append(*err, errs.Wrapf(closer.Close(), format, a...))
+	errC := closer.Close()
+	if errC == nil {
+		return
+	}
+
+	*err = multierror.Append(*err, errs.Wrapf(errC, format, a...))
 }
