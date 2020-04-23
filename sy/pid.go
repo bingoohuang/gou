7d9b757da650a2d6cb4636fe500ecbb17eb9e35d
bingoohuang <bingoo.huang@gmail.com>
2020-04-23 14:53:52 +0800

    UpdatePidFile

---
+++ sy/pid.go
@@ -0,0 +1,36 @@
+package sy
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/bingoohuang/gou/file"
+	"github.com/pkg/errors"
+)
+
+// UpdatePidFile update the pid to pidFile like var/pid (kill -USR2 {pid} 会执行重启)
+// If pidFile is empty, it will try env PID_FILE' value, then to "var/pid" file.
+func UpdatePidFile(pidFile string) error {
+	if pidFile == "" {
+		pidFile = os.Getenv("PID_FILE")
+	}
+
+	if pidFile == "" {
+		pidFile = "var/pid"
+	}
+
+	currentPID := strconv.Itoa(os.Getpid())
+	oldPid, err := file.ReadValue(pidFile, currentPID)
+
+	if err != nil {
+		return errors.Wrapf(err, "read pid file %s", pidFile)
+	}
+
+	if oldPid != currentPID {
+		if err := file.WriteValue(pidFile, currentPID); err != nil {
+			return errors.Wrapf(err, "write pid file %s", pidFile)
+		}
+	}
+
+	return nil
+}
